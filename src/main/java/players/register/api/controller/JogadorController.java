62f9f3258a3a01fd8c5fa4fb51aaeeedcd8de38d
Douglas <douglas_timaofiel@hotmail.com>
2023-08-03 14:40:08 -0300

    Funcionalidade de Exclusão

--- src/main/java/players/register/api/controller/JogadorController.java
+++ src/main/java/players/register/api/controller/JogadorController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,6 +44,12 @@ public class JogadorController {
 	public void atualizar(@RequestBody @Valid DadosAtualizacaoJogador dados) {
 		 var jogador = jogadorRepository.getReferenceById(dados.id());
 		 jogador.atualizarInformacoes(dados);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void excluir (@PathVariable Long id) {
+		 jogadorRepository.deleteById(id);
 	}
 	
 }
