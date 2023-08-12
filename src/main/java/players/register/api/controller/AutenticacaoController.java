b64f05944c6980ccf793af097ef15466c9d52427
Douglas <douglas_timaofiel@hotmail.com>
2023-08-11 23:49:08 -0300

    Gerando tokens JWT

--- src/main/java/players/register/api/controller/AutenticacaoController.java
+++ src/main/java/players/register/api/controller/AutenticacaoController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import players.register.api.domain.usuario.DadosAutenticacao;
+import players.register.api.domain.usuario.Usuario;
+import players.register.api.infra.security.TokenService;
 
 @RestController
 @RequestMapping("/login")
@@ -19,12 +21,15 @@ public class AutenticacaoController {
 	@Autowired
 	private AuthenticationManager manager;
 	
+	@Autowired
+	private TokenService tokenService;
+	
 	@PostMapping
 	public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
 		var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
 		var authentication = manager.authenticate(token);
 		
-		return ResponseEntity.ok().build();
+		return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
 	}  
 		
 }
